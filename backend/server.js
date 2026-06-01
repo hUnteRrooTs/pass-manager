7a93d81838a962664e66875f38f8ce87d62d36ef
TheUSer5050 <anwith5131@gmail.com>
2026-06-01 11:49:06 +0530

    Changed port number of SMTP

--- backend/server.js
+++ backend/server.js
@@ -442,8 +442,8 @@ async function createTransporter() {
   const transporter =
     nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       family: 4,
       auth: {
         user: process.env.GMAIL,
